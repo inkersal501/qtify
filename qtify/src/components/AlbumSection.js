9f6f270bf72c2647597da8deff13284342345e3c
inkersal501 <inkersal501@gmail.com>
2024-08-23 20:53:20 +0530

    Album cards

--- qtify/src/components/AlbumSection.js
+++ qtify/src/components/AlbumSection.js
@@ -8,7 +8,11 @@ function AlbumSection({albumsList, albumTitle}) {
   return ( 
 
     <Box sx={{backgroundColor: "#121212",padding:"20px" }}>
+        
+        <Box sx={{display:"flex",justifyContent:"space-between",padding:"0 30px"}}>
         <Typography className={styles.albumTitle} variant='h6'>{albumTitle}</Typography>
+            <button className={styles.actionBtn}>Collapse</button>
+        </Box>
         <Grid container spacing={2} sx={{marginLeft:"0px"}}>
             {
             albumsList.map(album =>(
